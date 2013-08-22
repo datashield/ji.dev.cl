21e4aeb40c00139873274725bab8d2bd9842b244
jisaeva <julii@IKBM-B19011444.ans.umb.no>
2013-08-22 12:01:17 +0200

    Datasets changed

--- test/tests.R
+++ test/tests.R
@@ -1,12 +1,14 @@
 # log in to opals
-o1<-opal.login('administrator', 'password', 'http://54.242.140.255')
-o2<-opal.login('administrator', 'password', 'http://54.242.46.59')
-opals<-list(o1, o2)
-names(opals)<-c('Study 1', 'Study 2')
+server1 <- opal.login('administrator', 'password', 'http://54.242.140.255')
+server2 <- opal.login('administrator', 'password', 'http://54.242.46.59')
+server3 <- opal.login('administrator', 'password', 'http://23.22.215.42')
+opals <- list(server1,server2,server3)
+names(opals) <- c("Study1", "Study2", "Study3")
 
 # assign data to opals
-datashield.assign(opals[[1]], 'D', 'HOPsim.hopsim1ob')
-datashield.assign(opals[[2]], 'D', 'HOPsim.hopsim2ob')
+datashield.assign(server1, 'D', 'CNSIM.CNSIM')
+datashield.assign(server2, 'D', 'CNSIM.CNSIM')
+datashield.assign(server3, 'D', 'CNSIM.CNSIM')
 
 # LOAD THE CLIENT FUNCTION (in Rstudio you just do 'build&reload' or do library(ji.dev.cl))
 # or do the same as below but give it the path to the R script
@@ -17,14 +19,14 @@ source("ji.ds.heatmapplot.R")
 library(fields)
 
 # GENERATE A COMBINED HEATMAP PLOT ACROSS THE STUDIES USING THE TWO VARIABLES BELOW
-ji.ds.heatmapplot(opals, quote(D$sbp), quote(D$glu), type="combine")
+ji.ds.heatmapplot(opals, quote(D$LAB_TSC), quote(D$LAB_HDL), type="combine")
 
 # GENERATE A SPLIT HEATMAP PLOT WHERE EACH STUDY IS PLOTTED SEPARATELY
 # THIS WAY THE USER HAS NOW MORE FLEXIBILITY
-ji.ds.heatmapplot(opals, quote(D$sbp), quote(D$glu), type="split")
+ji.ds.heatmapplot(opals, quote(D$LAB_TSC), quote(D$LAB_HDL), type="split")
 
 # GENERATE A HEATMAP PLOT A SPECIFIC STUDY, HERE THE 2ND STUDY ONLY IS PLOTTED 
-ji.ds.heatmapplot(opals[2], quote(D$sbp), quote(D$glu), type="split")
+ji.ds.heatmapplot(opals[2], quote(D$LAB_TSC), quote(D$LAB_HDL), type="split")
 
 # GENERATE A SPLIT HEATMAP PLOT WITH LESS DENSE GRID
-ji.ds.heatmapplot(opals, quote(D$sbp), quote(D$glu), type="split", numints=15)
+ji.ds.heatmapplot(opals, quote(D$LAB_TSC), quote(D$LAB_HDL), type="split", numints=15)
