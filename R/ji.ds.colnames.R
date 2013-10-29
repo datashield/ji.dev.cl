f83be7d8a52bcda02b3c56c4539ebbeb833af69b
jisaeva <juis@PC-60079.fhi.no>
2013-10-29 10:06:30 +0100

    Typo corrected

--- R/ji.ds.colnames.R
+++ R/ji.ds.colnames.R
@@ -41,23 +41,16 @@ ji.ds.colnames = function(datasources=NULL, x=NULL) {
   cally = call('class', x)
   classes_x = datashield.aggregate(datasources, cally)
   flag = 0  
-  for (i in 1:num.sources) {
+  for (i in 1:numsources) {
     if ( (classes_x[[i]]!='matrix') && (classes_x[[i]]!='data.frame') ){
       message("\n\n ALERT!\n")
       message(" Please provide a valid matrix-like object for study ",i, "\n")
       flag=1
     }
-    if (flag==1)
-      stop(" End of process!\n\n", call.=FALSE)
   }
-#   
-# 
-#   if ( (!is.matrix(x)) && (!is.data.frame(x)) ){
-#     message("\n\n ALERT!\n")
-#     message(" Please provide a valid matrix-like object\n")
-#     stop(" End of process!\n\n", call.=FALSE)
-#   }
-# 
+  if (flag==1)
+    stop(" End of process!\n\n", call.=FALSE)
+
   
   cally <- call('colnames', x )
   column_names <- datashield.aggregate(datasources, cally)
