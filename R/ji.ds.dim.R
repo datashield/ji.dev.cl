d8d459c40a60130cb9591f1f336ecde5e22a9887
jisaeva <juis@PC-60079.fhi.no>
2013-10-29 10:11:07 +0100

    Amended check for a matrix like object

--- R/ji.ds.dim.R
+++ R/ji.ds.dim.R
@@ -36,14 +36,21 @@ ji.ds.dim = function(datasources=NULL, x=NULL) {
     stop(" End of process!\n\n", call.=FALSE)
   }
   
-  #   num.sources=length(datasources)
-  #   for (i in 1:num.sources) {
-  #     if ( (!is.matrix(x)) && (!is.data.frame(x)) ){
-  #       message("\n\n ALERT!\n")
-  #       message(" Please provide a valid matrix-like object for study ",i, "\n")
-  #       stop(" End of process!\n\n", call.=FALSE)
-  #     }
-  #   }
+  
+  # check whether a given vector is a matrix like object
+  numsources = length(datasources)
+  cally = call('class', x)
+  classes_x = datashield.aggregate(datasources, cally)
+  flag = 0  
+  for (i in 1:numsources) {
+    if ( (classes_x[[i]]!='matrix') && (classes_x[[i]]!='data.frame') ){
+      message("\n\n ALERT!\n")
+      message(" Please provide a valid matrix-like object for study ",i, "\n")
+      flag=1
+    }
+  }
+  if (flag==1)
+    stop(" End of process!\n\n", call.=FALSE)
   
   
   cally <- call('dim', x )
